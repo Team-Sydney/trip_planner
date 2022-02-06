cde098c1e69f2fd2a0ef8fa53dbe1883471a6d69
Jacob Brasil <jnxtbrasil@gmail.com>
2022-02-05 21:45:49 -0500

    Added JSON parsing to User & Trip models

--- lib/backend/models/trip.dart
+++ lib/backend/models/trip.dart
@@ -1,16 +1,21 @@
+import 'package:trip_planner/backend/models/User.dart';
+
 class Trip {
   final int id;
   String _title;
   final String destination;
   DateTime _startDate;
   DateTime _endDate;
+  List<User> _users;
 
   Trip.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         destination = json['destination'],
         _title = json['title'],
         _startDate = DateTime.parse(json['startDate']),
-        _endDate = DateTime.parse(json['endDate']);
+        _endDate = DateTime.parse(json['endDate']),
+        _users =
+            (json['users'] as List).map((user) => User.fromJson(user)).toList();
 
   Map<String, dynamic> toJson() => {
         'id': id,
@@ -18,6 +23,7 @@ class Trip {
         'title': _title,
         'startDate': _startDate.toIso8601String(),
         'endDate': _endDate.toIso8601String(),
+        'users': _users,
       };
 
   set title(String title) {
@@ -37,4 +43,10 @@ class Trip {
   }
 
   DateTime get endDate => _endDate;
+
+  set users(List<User> users) {
+    _users = users;
+  }
+
+  List<User> get users => _users;
 }
--- lib/backend/models/user.dart
+++ lib/backend/models/user.dart
@@ -2,18 +2,50 @@ import 'package:trip_planner/backend/models/trip.dart';
 
 class User {
   final int id;
-  String firstName;
-  String lastName;
-  String email;
-  List<Trip> trips;
+  String _firstName;
+  String _lastName;
+  String _email;
+  List<Trip> _trips;
 
-  User({
-    required this.id,
-    required this.firstName,
-    required this.lastName,
-    required this.email,
-    this.trips = const [],
-  });
+  User.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        _firstName = json['first_name'],
+        _lastName = json['last_name'],
+        _email = json['email'],
+        _trips =
+            (json['trips'] as List).map((trip) => Trip.fromJson(trip)).toList();
 
-  String get name => "$firstName $lastName";
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'first_name': _firstName,
+        'last_name': _lastName,
+        'email': _email,
+        'trips': _trips,
+      };
+
+  String get name => '$firstName $lastName';
+
+  set firstName(String firstName) {
+    _firstName = firstName;
+  }
+
+  String get firstName => _firstName;
+
+  set lastName(String lastName) {
+    _lastName = lastName;
+  }
+
+  String get lastName => _lastName;
+
+  set email(String email) {
+    _email = email;
+  }
+
+  String get email => _email;
+
+  set trips(List<Trip> trips) {
+    _trips = trips;
+  }
+
+  List<Trip> get trips => _trips;
 }
